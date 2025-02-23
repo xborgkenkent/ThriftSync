969985fecf1f5d10a5539eca4ac76551a5972c17
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:21 +0800

    Added goal entity

---
+++ ThriftSync/Domain/Entities/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Goal
+{
+    [Key]
+    public Guid Id { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public decimal TargetAmount { get; set; }
+    public decimal CurrentAmount { get; set; }
+    public DateTime Deadline { get; set; }
+}
