9e0ee146e1d4e33570817dfacceca0b09919994e
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:47:08 +0800

    Update program

--- ThriftSync/Program.cs
+++ ThriftSync/Program.cs
@@ -1,23 +1,30 @@
+using ThriftSync.Infrastructure.Data;
+using ThriftSync.Infrastructure.Repositories;
+using ThriftSync.Application.Services;
+using ThriftSync.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
