96530043ee8eba4d217da7f6eecc1a28fd486dac
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:46:12 +0800

    Added initial appdbcontext

---
+++ ThriftSync/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ThriftSync.Infrastructure.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Income> Incomes { get; set; }
+    public DbSet<Budget> Budgets { get; set; }
+    public DbSet<Goal> Goals { get; set; }
+    public DbSet<Report> Reports { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
