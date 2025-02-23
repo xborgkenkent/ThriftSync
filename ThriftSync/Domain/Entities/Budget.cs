4af95feff71d5b24e20c907c2e4e58bb2b7d9c61
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:44:58 +0800

    Added budget entity

---
+++ ThriftSync/Domain/Entities/Budget.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Budget
+{
+    [Key]
+    public Guid Id { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId { get; set; }
+    public decimal Amount { get; set; }
+    public Period Period { get; set; } 
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
+
+public enum Period
+{
+    Monthly,
+    Quarterly,
+    Annually
+}
