c4d4ca8a63ff71383ca4a1f1a74bc19680af68e8
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:46:26 +0800

    Added appdbcontext factory

---
+++ ThriftSync/Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace ThriftSync.Infrastructure.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class AppDbContextFactory: IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
