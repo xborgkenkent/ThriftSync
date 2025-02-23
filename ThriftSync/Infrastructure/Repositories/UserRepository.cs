9c8bfa38cbacdc2b43180cec3dba34675b6f057d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:46:40 +0800

    Added user repo

---
+++ ThriftSync/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+using ThriftSync.Infrastructure.Data;
+using BCrypt.Net;
+namespace ThriftSync.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+
+    private readonly AppDbContext _context;
+    
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<User> AddUserAsync(User user)
+    {
+        user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(user.PasswordHash);
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public Task<User> UpdateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public Task<bool> ValidateUserCredentialsAsync(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
