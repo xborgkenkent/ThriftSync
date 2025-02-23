46499f76a1d0910cf5bb0c41628bf67026f352c6
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:42 +0800

    Added user entity

---
+++ ThriftSync/Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThriftSync.Domain.Entities;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public DateTime LastLogin { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
