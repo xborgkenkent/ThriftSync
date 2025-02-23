aa3000b979428d91cac74096c90262fbe7952343
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:27 +0800

    Added income entity

---
+++ ThriftSync/Domain/Entities/Income.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Income
+{
+    [Key]
+    public Guid Id { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+}
