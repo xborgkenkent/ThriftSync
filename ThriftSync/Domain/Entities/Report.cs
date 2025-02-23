e993f049fff042bb0c3d88998693f47261602419
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:35 +0800

    Added re[port entity

---
+++ ThriftSync/Domain/Entities/Report.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Report
+{
+    [Key]
+    public Guid Id { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    public ReportType Type { get; set; }
+    public DateTime GeneratedAt { get; set; }
+    public ReportFormat Format { get; set; }
+    public string Path { get; set; }
+}
+
+public enum ReportType
+{
+    Monthly, Quarterly, Yearly, Custom
+}
+
+public enum ReportFormat
+{
+    PDF, CSV
+}
