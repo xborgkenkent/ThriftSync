d6f55e5919e167842b6329eb6f18d39be130b9be
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:13 +0800

    Added expense entity

---
+++ ThriftSync/Domain/Entities/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Expense
+{
+    [Key]
+    public Guid Id { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Tags { get; set; }
+}
