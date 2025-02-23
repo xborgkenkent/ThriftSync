4ff87c52879392998b5afa69fb03245fb78c28fb
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:06 +0800

    Added category entity

---
+++ ThriftSync/Domain/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThriftSync.Domain.Entities;
+
+public class Category
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public CategoryType Type { get; set; }
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    public string Color { get; set; }
+    public string Icon { get; set; }
+}
+
+public enum CategoryType
+{
+    Expense,
+    Income
+}
