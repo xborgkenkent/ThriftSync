1883a76527650cd9bb2118387dc05e1ddfe04a5d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-23 13:45:58 +0800

    Added user repo interface

---
+++ ThriftSync/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User> AddUserAsync(User user);
+    Task<User> UpdateUserAsync(User user);
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<bool> ValidateUserCredentialsAsync(string email, string password);
+}
