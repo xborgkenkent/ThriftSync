e421bb7ec1e853ede86a2c023d96f7c7b954dc5f
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 12:33:58 +0800

    Added login function

--- ThriftSync/Application/Services/AuthService.cs
+++ ThriftSync/Application/Services/AuthService.cs
@@ -1,14 +1,24 @@
-﻿using ThriftSync.Domain.Interfaces;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using ThriftSync.Domain.Interfaces;
 using ThriftSync.Domain.Entities;
+using ThriftSync.Infrastructure.Security;
+
 namespace ThriftSync.Application.Services;
 
 public class AuthService
 {
     private readonly IUserRepository _userRepository;
-
-    public AuthService(IUserRepository userRepository)
+    private readonly IConfiguration _configuration;
+    private readonly JwtService _jwtService;
+    
+    public AuthService(IUserRepository userRepository, IConfiguration configuration, JwtService jwtService)
     {
         _userRepository = userRepository;
+        _configuration = configuration;
+        _jwtService = jwtService;
     }
 
     public async Task<User?> RegisterUserAsync(string email, string username, string password)
@@ -23,4 +33,16 @@ public class AuthService
         
         return await _userRepository.AddUserAsync(newUser);
     }
+
+    public async Task<string?> LoginUserAsync(string email, string password)
+    {
+        var user = await _userRepository.GetUserByEmailAsync(email);
+        if (user == null || !await _userRepository.ValidateUserCredentialsAsync(user.PasswordHash, password))
+        {
+            return null;
+        }
+
+        return _jwtService.GenerateJwtToken(user);
+    }
+
 }
