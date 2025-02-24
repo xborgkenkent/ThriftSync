1c505ef3fd8d613e068a2d0029600e10ea7b868a
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:45 +0800

    Added default category service

---
+++ ThriftSync/Application/Services/DefaultCategoryService.cs
@@ -0,0 +1,24 @@
+﻿using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+
+namespace ThriftSync.Application.Services;
+
+public class DefaultCategoryService
+{
+    private readonly IDefaultCategoryRepository _defaultCategoryRepository;
+
+    public DefaultCategoryService(IDefaultCategoryRepository defaultCategoryRepository)
+    {
+        _defaultCategoryRepository = defaultCategoryRepository;
+    }
+
+    public async Task<IEnumerable<DefaultCategory>> GetDefaultCategoriesAsync()
+    {
+        return await _defaultCategoryRepository.GetCategories();
+    }
+
+    public async Task<DefaultCategory?> GetDefaultCategoryByIdAsync(Guid id)
+    {
+        return await _defaultCategoryRepository.GetCategoryById(id);
+    }
+}
