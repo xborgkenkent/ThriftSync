13b84d5395eb41d85a5a2c7ad5634901f71bbe65
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:49:32 +0800

    Added goal repo

---
+++ ThriftSync/Infrastructure/Repositories/GoalRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+using ThriftSync.Infrastructure.Data;
+
+namespace ThriftSync.Infrastructure.Repositories;
+
+public class GoalRepository : IGoalRepository
+{
+    private readonly AppDbContext _context;
+
+    public GoalRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Goal?> GetGoalByUserIdAndGoaldIdAsync(Guid userId, Guid goalId)
+    {
+        var goal = await _context.Goals.Where(x => x.UserId == userId && x.Id == goalId).FirstOrDefaultAsync();
+        return goal;
+    }
+
+    public async Task<Goal> AddGoalAsync(Goal goal)
+    {
+        _context.Goals.Add(goal);
+        await _context.SaveChangesAsync();
+        return goal;
+    }
+
+    public async Task<IEnumerable<Goal>> GetGoalsByUserIdAsync(Guid userId)
+    {
+        var goals = await _context.Goals.Where(x => x.UserId == userId).ToListAsync();
+        return goals;
+    }
+}
