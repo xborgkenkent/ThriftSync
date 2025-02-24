04ad0ef21c8cf2836b041a43772628ced9f6800b
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:40 +0800

    Added category service

---
+++ ThriftSync/Application/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+
+namespace ThriftSync.Application.Services;
+
+public class CategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Category> AddCategoryAsync(Category category)
+    {
+        return await _categoryRepository.AddCategoryAsync(category);
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesByUserIdAsync(Guid userId)
+    {
+        return await _categoryRepository.GetCategoriesByUserIdAsync(userId);
+    }
+
+    public async Task<Category?> GetCategoryByUserIdAndCategoryIdAsync(Guid userId, Guid categoryId)
+    {
+        return await _categoryRepository.GetCategoryByUserIdAndCategoryIdAsync(userId, categoryId);
+    }
+
+    public Task<Category> UpdateCategoryAsync(Category category)
+    {
+        return _categoryRepository.UpdateCategoryAsync(category);
+    }
+}
