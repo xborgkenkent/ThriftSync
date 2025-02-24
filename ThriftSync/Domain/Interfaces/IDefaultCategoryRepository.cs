9f902a8954f4d680a233f836e81ccebf6865e1fd
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:34 +0800

    Added default category interface

---
+++ ThriftSync/Domain/Interfaces/IDefaultCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.Domain.Interfaces;
+
+public interface IDefaultCategoryRepository
+{
+    Task<IEnumerable<DefaultCategory>> GetCategories();
+    Task<DefaultCategory?> GetCategoryById(Guid id);
+}
