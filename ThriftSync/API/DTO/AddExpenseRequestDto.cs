56eef0d8a38a13592605243bb095624159cf58c2
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:26 +0800

    Added expense request dto

---
+++ ThriftSync/API/DTO/AddExpenseRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ThriftSync.API.DTO;
+
+public class AddExpenseRequestDto
+{
+    public Guid CategoryId { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Tags { get; set; }
+}
