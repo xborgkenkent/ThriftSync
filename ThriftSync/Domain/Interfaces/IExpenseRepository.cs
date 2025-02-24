98f09599f3fcb99bf2d88ed8b49b874803d194a2
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:44 +0800

    Added expense interface

---
+++ ThriftSync/Domain/Interfaces/IExpenseRepository.cs
@@ -0,0 +1,15 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.Domain.Interfaces;
+
+public interface IExpenseRepository
+{
+    Task<Expense> AddExpenseAsync(Expense expense);
+    Task<IEnumerable<Expense>> GetExpensesAsync();
+    Task<Expense?> GetExpenseByUserIdAndExpenseIdAsync(Guid userId, Guid expenseId);
+    Task<IEnumerable<Expense>> GetExpensesByUserIdAsync(Guid userId);
+    Task<IEnumerable<Expense>> GetExpensesByCategoryIdAsync(Guid categoryId);
+    Task<IEnumerable<Expense>> GetExpensesByUserIdAndCategoryIdAsync(Guid userId, Guid categoryId);
+    Task UpdateExpenseAsync(Expense expense);
+    Task DeleteExpenseAsync(Expense expense);
+}
