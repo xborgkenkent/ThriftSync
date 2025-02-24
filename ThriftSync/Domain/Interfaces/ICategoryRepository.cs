9ffce32fdf7b1c6a5b6d2add149bc00c12e7d497
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:26 +0800

    Added category interface

---
+++ ThriftSync/Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.Domain.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<Category> AddCategoryAsync(Category category);
+    Task<IEnumerable<Category>> GetCategoriesByUserIdAsync(Guid userId);
+    Task<Category?> GetCategoryByUserIdAndCategoryIdAsync(Guid userId, Guid categoryId);
+    Task<Category> UpdateCategoryAsync(Category category);
+}
