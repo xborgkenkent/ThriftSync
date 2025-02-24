7fbe481f153bc33dbac95a1bac5dd4b19a7e3edd
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:32 +0800

    Added goal request dto

---
+++ ThriftSync/API/DTO/AddGoalRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ThriftSync.API.DTO;
+
+public class AddGoalRequestDto
+{
+    public string Title { get; set; }
+    public decimal TargetAmount { get; set; }
+    public decimal CurrentAmount { get; set; }
+    public DateTime Deadline { get; set; }
+}
