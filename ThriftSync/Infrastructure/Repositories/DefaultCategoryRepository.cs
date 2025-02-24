5dde222bbb4e5c3607492204ffc6e3aa8e98fada
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:49:20 +0800

    Added default category repo

---
+++ ThriftSync/Infrastructure/Repositories/DefaultCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+using ThriftSync.Infrastructure.Data;
+
+namespace ThriftSync.Infrastructure.Repositories;
+
+public class DefaultCategoryRepository : IDefaultCategoryRepository
+{
+    private readonly AppDbContext _context;
+
+    public DefaultCategoryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<DefaultCategory>> GetCategories()
+    {
+        var categories = await _context.DefaultCategories.ToListAsync();
+        return categories;
+    }
+
+    public async Task<DefaultCategory?> GetCategoryById(Guid id)
+    {
+        var category = await _context.DefaultCategories.FirstOrDefaultAsync(c => c.Id == id);
+        return category;
+    }
+}
