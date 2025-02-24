900a264957d8aff28ff81418e45aaa84ac6bda1f
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:50 +0800

    Added goal interface

---
+++ ThriftSync/Domain/Interfaces/IGoalRepository.cs
@@ -0,0 +1,10 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.Domain.Interfaces;
+
+public interface IGoalRepository
+{
+    Task<Goal?> GetGoalByUserIdAndGoaldIdAsync(Guid userId, Guid goalId);
+    Task<Goal> AddGoalAsync(Goal goal);
+    Task<IEnumerable<Goal>> GetGoalsByUserIdAsync(Guid userId);
+}
