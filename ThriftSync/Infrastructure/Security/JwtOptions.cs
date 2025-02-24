135a3be66a86b7b096e2411b2e1f454453d6be6b
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 12:34:49 +0800

    Added jwtoptions

---
+++ ThriftSync/Infrastructure/Security/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ThriftSync.Infrastructure.Security;
+
+public class JwtOptions
+{
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+}
