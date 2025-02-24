80274bd8f7f209abb3f47d214b1bd1c3ed4248b8
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:16 +0800

    Added default category entity

---
+++ ThriftSync/Domain/Entities/DefaultCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ThriftSync.Domain.Entities;
+
+public class DefaultCategory
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; } // "Income" or "Expense"
+    public string IconUrl { get; set; }
+}
