ee057ef13ca1a8d99b4a9bd5fab402fed9c2af05
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:54 +0800

    Added expense service

---
+++ ThriftSync/Application/Services/ExpenseService.cs
@@ -0,0 +1,35 @@
+﻿using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+
+namespace ThriftSync.Application.Services;
+
+public class ExpenseService
+{
+    private readonly IExpenseRepository _expenseRepository;
+
+    public ExpenseService(IExpenseRepository expenseRepository)
+    {
+        _expenseRepository = expenseRepository;
+    }
+
+    public async Task<Expense> AddExpenseAsync(Expense expense)
+    {
+        return await _expenseRepository.AddExpenseAsync(expense);
+    }
+
+    public async Task<Expense?> GetExpenseByUserIdAndExpenseIdAsync(Guid userId, Guid expenseId)
+    {
+        return await _expenseRepository.GetExpenseByUserIdAndExpenseIdAsync(userId, expenseId);
+    }
+
+    public async Task<IEnumerable<Expense>> GetExpensesByUserIdAsync(Guid userId)
+    {
+        return await _expenseRepository.GetExpensesByUserIdAsync(userId);
+    }
+
+    
+    public async Task<IEnumerable<Expense>> GetExpensesByUserIdAndCategoryIdAsync(Guid userId, Guid categoryId)
+    {
+        return await _expenseRepository.GetExpensesByUserIdAndCategoryIdAsync(userId, categoryId);
+    }
+}
