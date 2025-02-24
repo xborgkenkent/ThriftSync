fc4dffa941ceb0b0f0f769e076be0911af5505c5
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:48:01 +0800

    Added goal service

---
+++ ThriftSync/Application/Services/GoalService.cs
@@ -0,0 +1,30 @@
+﻿using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+using ThriftSync.Infrastructure.Repositories;
+
+namespace ThriftSync.Application.Services;
+
+public class GoalService
+{
+    private readonly IGoalRepository _goalRepository;
+
+    public GoalService(IGoalRepository goalRepository)
+    {
+        _goalRepository = goalRepository;
+    }
+
+    public async Task<IEnumerable<Goal>> GetGoalsAsync(Guid userId)
+    {
+        return await _goalRepository.GetGoalsByUserIdAsync(userId);
+    }
+
+    public async Task<Goal> AddGoalAsync(Goal goal)
+    {
+        return await _goalRepository.AddGoalAsync(goal);
+    }
+
+    public async Task<Goal?> GetGoalByUserIdAndGoaldIdAsync(Guid userId, Guid goalId)
+    {
+        return await _goalRepository.GetGoalByUserIdAndGoaldIdAsync(userId, goalId);
+    }
+}
