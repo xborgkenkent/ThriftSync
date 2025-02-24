522442f40485b2cb8aa5396d90f165737ed3d640
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 12:33:32 +0800

    Added login endpoint

--- ThriftSync/API/Controllers/AuthController.cs
+++ ThriftSync/API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ThriftSync.API.DTO;
 using ThriftSync.Application.Services;
 
@@ -13,6 +14,7 @@ public class AuthController: ControllerBase
     {
         _authService = authService;
     }
+    
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerDto)
     {
@@ -20,5 +22,24 @@ public class AuthController: ControllerBase
         if(user == null) return BadRequest(new { message = "Account already exists" });
 
         return Created();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto loginDto)
+    {
+        Console.WriteLine($"Email: {loginDto.Email}, Password: {loginDto.Password}");
+
+        var token = await _authService.LoginUserAsync(loginDto.Email, loginDto.Password);
+        if (token == null) return Unauthorized(new { message = "Invalid credentials" });
+        
+        Response.Cookies.Append("TS", token, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddHours(2)
+        });
+        
+        return Ok();
     }
 }
