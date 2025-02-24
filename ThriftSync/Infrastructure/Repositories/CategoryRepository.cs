3a49914f25476fcd4eba3175a6d1f91be64a18c9
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:49:13 +0800

    Added category repo

---
+++ ThriftSync/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftSync.Domain.Entities;
+using ThriftSync.Domain.Interfaces;
+using ThriftSync.Infrastructure.Data;
+
+namespace ThriftSync.Infrastructure.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _context;
+    public CategoryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Category> AddCategoryAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesByUserIdAsync(Guid userId)
+    {
+        var categories =  await _context.Categories.Where(x => x.UserId == userId).ToListAsync();
+        return categories;
+    }
+
+    public async Task<Category?> GetCategoryByUserIdAndCategoryIdAsync(Guid userId, Guid categoryId)
+    {
+        var categories =  await _context.Categories.Where(x => x.UserId == userId).FirstOrDefaultAsync();
+        return categories;
+    }
+
+    public Task<Category> UpdateCategoryAsync(Category category)
+    {
+        throw new NotImplementedException();
+    }
+}
