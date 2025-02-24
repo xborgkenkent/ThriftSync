0d485492213732fb5ca7203f1eab655737c34548
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-24 21:47:17 +0800

    Added category request dto

---
+++ ThriftSync/API/DTO/AddCategoryRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using ThriftSync.Domain.Entities;
+
+namespace ThriftSync.API.DTO;
+
+public class AddCategoryRequestDto
+{
+    public string Name { get; set; }
+    public CategoryType Type { get; set; }
+    public string Color { get; set; }
+    public Guid CategoryId { get; set; }
+}
